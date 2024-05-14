352bb242ae5eb663a5623d7ea51d54fa173ee8a3
Hadeer Adel <119612208+Hadeer-Adel729@users.noreply.github.com>
2024-05-14 10:33:35 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,18 +1,12 @@
 pipeline {
     agent any
-
+    
     stages {
-        stage('Checkout') {
+        stage('Execute ls command') {
             steps {
-                // Checkout your source code from version control
-                git 'https://github.com/Hadeer-Adel729/CloudTask.git'
-            }
-        }
-
-        stage('Execute Bash Script') {
-            steps {
-                // Execute your bash script
-                sh './execute_ls.sh'
+                script {
+                    sh 'bash execute_is.sh'
+                }
             }
         }
     }
