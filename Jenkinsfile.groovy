650d1722d24103402e4fe357d9a654344351bb07
Hadeer Adel <119612208+Hadeer-Adel729@users.noreply.github.com>
2024-05-14 11:52:48 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,17 +1,11 @@
 pipeline {
     agent any
-    
     stages {
-        stage('Fetch and Execute') {
+        stage('Execute ls command') {
             steps {
-                // Checkout the main branch to access Jenkinsfile
-                checkout([$class: 'GitSCM', branches: [[name: 'main']], userRemoteConfigs: [[url: 'https://github.com/Hadeer-Adel729/CloudTask.git']]])
-
-                // Checkout the master branch to access execute_ls.sh
-                checkout([$class: 'GitSCM', branches: [[name: 'master']], userRemoteConfigs: [[url: 'https://github.com/Hadeer-Adel729/CloudTask.git']]])
-
-                // Execute the execute_ls.sh script
-                sh './execute_ls.sh'
+                script {
+                    bat 'dir execute_ls.sh'
+                }
             }
         }
     }
