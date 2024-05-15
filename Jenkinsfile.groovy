0c2e5c20d313adb4c5d5198f6ebdc6ad5a9ffb4d
Hadeer Adel <119612208+Hadeer-Adel729@users.noreply.github.com>
2024-05-15 17:51:36 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,12 +1,1 @@
-pipeline {
-    agent any
-    stages {
-        stage('Execute ls command') {
-            steps {
-                script {
-                    bat 'dir execute_ls.sh'
-                }
-            }
-        }
-    }
-}
+.
